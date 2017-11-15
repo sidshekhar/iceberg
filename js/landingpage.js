1c467b3cd5241e1fc5d8ba96832c7dac1dcd4a54
sidshekhar24 <ss2744@cornell.edu>
2017-11-15 20:05:41 +0000

    activate

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,8 +1,14 @@
 function thankYou() {
-	if (document.getElementById('email').value != '';) && (document.getElementById('email2').value != '';){
+	if (document.getElementById('email').value != '' || document.getElementById('email2').value != '') {
 		$('#thankyoumodal').modal('show');
-	document.getElementById('email').value = '';
-	document.getElementById('email2').value = '';
+		document.getElementById('email').value = '';
+		document.getElementById('email2').value = '';
 	}
-	
+}
+
+function activation() {
+	// var activationcode = document.getElementById('invite');
+	// if (activationcode == '3b23c9') {
+	window.location = 'https://www.sidshekhar.com/iceberg/welcome.html';
+	// }
 }
