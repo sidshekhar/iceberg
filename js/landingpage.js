d1d6a927b8da83fe9661151bf9c170b365dc5af2
sidshekhar24 <ss2744@cornell.edu>
2017-11-15 20:18:00 +0000

    activate

--- js/landingpage.js
+++ js/landingpage.js
@@ -8,7 +8,19 @@ function thankYou() {
 
 function Activation() {
 	var activationcode = document.getElementById('invite').value;
-	if (activationcode == '3b23c9') {
+	if (activationcode == '25acb1') {
+		window.location = 'john.html';
+	} else if (activationcode == '6b405e') {
+		window.location = 'aaron.html';
+	} else if (activationcode == 'f256b2') {
+		window.location = 'brandon.html';
+	} else if (activationcode == '591833') {
+		window.location = 'kyle.html';
+	} else if (activationcode == '50ee09') {
+		window.location = 'nick.html';
+	} else if (activationcode == '475f39') {
+		window.location = 'nicholas.html';
+	} else if (activationcode == '3b23c9') {
 		window.location = 'welcome.html';
 	}
 }
