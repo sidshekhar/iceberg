9281932f0c133293593ec76774ee107d563e7690
sidshekhar24 <ss2744@cornell.edu>
2017-11-15 18:08:31 +0000

    button sizing

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,4 +1,5 @@
 function thankYou() {
+	// if (document.getElementById('email').value = '';)
 	$('#thankyoumodal').modal('show');
 	document.getElementById('email').value = '';
 	document.getElementById('email2').value = '';
